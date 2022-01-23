2f78507f4b037587f5fba0f93b9baa1bce5b7cd2
Firere <67512865+Firere@users.noreply.github.com>
2022-01-23 01:32:23 +0000

    Update index.php a bit
    Added the scanning of sites and basic iteration of them in the page itself

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -1,9 +1,26 @@
 <?php
-/*
- * This variable assumes you're running Nginx on Ubuntu 20.04,
- * but if your path for enabled sites is different you may modify this
-*/
-$sitesEnabled = "/etc/nginx/sites-enabled/";
+// This variable assumes you're running Nginx on Ubuntu 20.04,
+// but if your path for enabled sites is different you may modify this
+$sitesEnabledPath = "./test";
+
+// scandir() has 3 parameters, the last of which is context;
+// it can only be nullable in version >=8.0.0
+$sitesEnabled = scandir($sitesEnabledPath);
+
+# to remove empty "." and ".." directories
+array_splice($sitesEnabled, 0, 2);
+
+
+// This only works (properly) if your configuration files end in conf;
+// to edit each file to show only the domain, you will have to implement
+// something yourself, though if the files are the domain itself you don't
+// have to do anything
+foreach ($sitesEnabled as &$site) {
+	$site = substr($site, 0, -5);
+}
+
+# For why this is done, see: https://bugs.php.net/bug.php?id=60534
+unset($site);
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,6 +29,19 @@ $sitesEnabled = "/etc/nginx/sites-enabled/";
 </head>
 <body>
 	<h1 id="header-ip"><h1>
+	<hr/>
+	<div id="sites">
+		<?php
+			foreach ($sitesEnabled as $site) {
+				echo
+"
+		<div class=\"site-container\" id=\"" . $site . "\">
+			" . $site . "
+		</div>	
+";
+			}
+		?>
+	</div>
 	<script>
 		window.onload = function() {
 			const serverIp = window.location.hostname;
